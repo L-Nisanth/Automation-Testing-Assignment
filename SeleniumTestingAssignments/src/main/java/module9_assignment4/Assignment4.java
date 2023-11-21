7898d9d0c6acb03f13437594162ccfb8823ed941
ADMIN <ADMIN@DESKTOP-EHFVP40>
2023-11-21 19:03:53 +0530

    finished module 9

--- SeleniumTestingAssignments/src/main/java/module9_assignment4/Assignment4.java
+++ SeleniumTestingAssignments/src/main/java/module9_assignment4/Assignment4.java
@@ -1,8 +1,11 @@
-package module9_assignment1;
+package module9_assignment4;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,15 +14,22 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Assignment1 {
-
-	// ****************** Module 9: Assignment - 1 *************************
-
-	// Write a Test Script to Automate www.saucedemo.com Login in TestNG - Use Hard Coded Values.
-	
+public class Assignment4 {
+	// ****************** Module 9: Assignment - 4 *************************
+	/*
+	 * Write a Test Script to Automate www.saucedemo.com Login in TestNG - Pass
+	 * parameters from .properties file.
+	 */
 
 	WebDriver driver;
-
+    
+	public static Properties loadData() throws IOException {
+		FileInputStream file = new FileInputStream(".\\src\\main\\java\\module9_assignment4\\loginData.properties");
+		Properties prop = new Properties();
+		prop.load(file);
+		return prop;
+		
+	}
 	
 	@BeforeTest
 	public void goToUrl() {
@@ -28,14 +38,14 @@ public class Assignment1 {
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
+        
 	}
 
 	@Test
-	public void Login() {
+	public void Login() throws IOException {
 		assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/");
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("user-name")).sendKeys(loadData().getProperty("userName"));
+		driver.findElement(By.id("password")).sendKeys(loadData().getProperty("password"));
 		driver.findElement(By.id("login-button")).click();
 		assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html");
 		
@@ -46,5 +56,10 @@ public class Assignment1 {
 	public void quitBrowser() {
 		driver.quit();
 	}
-
+	
+	
+	
+	
+	
+	
 }
